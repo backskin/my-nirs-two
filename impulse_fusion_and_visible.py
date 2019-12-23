ca53eeb7d206006cd03acb7ced2dea7544d08a33
Vlad Bashkin <vbackskin@gmail.com>
2019-12-23 04:43:21 +0400

    пустые скрипты. и все надо заполнить

--- impulse_fusion_and_visible.py
+++ impulse_fusion_and_visible.py
@@ -5,7 +5,7 @@ from additions import get_imp_noise
 from fusions import fusion_impulse_filter
 
 
-def cycle(dens: float, amount_of_images: int, wm_image: Image.Image, orig_image: Image.Image):
+def cycle(name: str, dens: float, amount_of_images: int, wm_image: Image.Image, orig_image: Image.Image):
     images = []
     for i in range(amount_of_images):
         next_image = get_imp_noise(dens, orig_image)
@@ -13,11 +13,19 @@ def cycle(dens: float, amount_of_images: int, wm_image: Image.Image, orig_image:
         images.append(next_image)
 
     restored_image = fusion_impulse_filter(images)
-    restored_image.save('restored_with_visual_watermark.bmp')
+    restored_image.save(name + '_restored_with_visual_watermark.bmp')
 
 
 folder = 'examples/'
+out_folder = 'outputs/'
 image_name = 'pineapple'
 original: Image.Image = Image.open(folder + image_name + '.bmp')
 watermark: Image.Image = Image.open(folder + 'wm.bmp')
-#test: 20 images
+
+# test: 20 images
+img_amount = 20
+start = 0.1
+one_step = 0.1
+steps = 10
+for iteration in range(steps):
+    cycle(out_folder+image_name, start + iteration * one_step, img_amount, original, watermark)
